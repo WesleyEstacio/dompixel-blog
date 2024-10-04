476d35fb45419fd1f150da5269a6e6e002dbde33
Wesley Estacio <wesleyestacio4@gmail.com>
2024-10-04 13:59:57 -0300

    feat: initial project settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.devtool = 'source-map'
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
